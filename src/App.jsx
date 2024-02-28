999d01f062df0b66bdbf98919a2f8e35a0434451
Gaspare Tortora <g.tortora2@gmail.com>
2024-02-28 00:05:48 -0500

    Configured component architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import Ip from './components/Ip'
-import './App.css'
+import SearchBar from './components/SearchBar'
+import Map from './components/Map'
 
 function App() {
+  const [ipAddress, setIpAddress] = useState("")
+  const [error, setError] = useState("")
+
+  const handleChange = (data) => {
+    setIpAddress(data)
+  }
 
   return (
     <>
-      <Ip/>
+    <div className="header h-1/3">
+      <SearchBar setIpAddress={handleChange} setError={setError}/>
+      <div className="flex gap-24 bg-white py-16 px-32 absolute left-1/2 top-40 -translate-x-1/2 rounded-xl shadow-lg w-[70vw] min-w-fit justify-center">
+        <Ip ipAddress={ipAddress} err={error}/>
+      </div>
+    </div>
+    <Map />
     </>
   )
 }
