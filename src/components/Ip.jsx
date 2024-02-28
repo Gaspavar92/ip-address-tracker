20958dd55abe9c6f6f799ac0788d844ca0c46a8f
Gaspare Tortora <g.tortora2@gmail.com>
2024-02-28 00:06:27 -0500

    Created component for the Ip address API

--- src/components/Ip.jsx
+++ src/components/Ip.jsx
@@ -1,23 +1,67 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const Ip =  () =>  {
+const Ip =  ({ipAddress, err}) =>  {
+
+    const [response, setResponse] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
 
     // wrapping API call to get IP address in useEffect
     useEffect(() => {
         const fetchData = async () => {
-            const url = "https://geo.ipify.org/api/v2/country?apiKey=at_mnkebYNmNmi3huij17aFstlEooUbW&ipAddress=8.8.8.8";
-            axios.get(url).
-            then(response => {
-                console.log(response)
-            })
+                setLoading(true)
+                const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_IP_API_KEY}`;
+                axios.get(url, {
+                    params: {
+                        ipAddress: ipAddress
+                    }
+                }).
+                then(response => {
+                    setError("")
+                    setLoading(false);
+                    setResponse(response.data)
+                }).catch((error) => {
+                    console.log("something")
+                    setError(error.response.data.messages)
+                }).finally(() => {
+                    setLoading(false);
+                })
         }
-        fetchData()
-    }, [])
+        if (ipAddress) {
+            fetchData()
+        }
+    }, [ipAddress])
 
-    return (
-        <></>
-    )
+    if (loading) {
+        return <p>Loading...</p>;
+      }
+    
+      
+    if (error) {
+        return <p>{error}</p>
+    }
+
+    if (err) {
+        return <p>{err}</p>
+    }
+    
+    if (!ipAddress && !error) {
+        return <p>Look for an IP address to populate results.</p>
+    }
+
+        return (
+          
+     response.location && (
+            <>
+                <p className="border-r border-gray pr-16">Country: {response.location.country}</p>
+                <p className="border-r border-gray pr-16">City: {response.location.city}</p>
+                <p className="border-r border-gray pr-16">Region: {response.location.region}</p>
+                <p>ISP: {response.isp}</p>
+            </>
+      )
+          
+        );
 }
 
 export default Ip;
