eb286d1415bf8e1c099435c22de1aa40e73e8fa7
Gaspare Tortora <g.tortora2@gmail.com>
2024-03-01 11:18:16 -0500

    Fixed error

--- src/components/NewLoc.jsx
+++ src/components/NewLoc.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react';
 import { useMapEvents } from 'react-leaflet'
 
 function NewLoc() {
-
-    const [coord, setCoord] = useState({})
     const map = useMapEvents({
       click: (e) => {
-        setCoord(e.latlng)
+        map.setView([e.latlng.lat, e.latlng.lng])
       }
     })
-    map.setView([coord.lat, coord.lng])
     return null
   }
 
