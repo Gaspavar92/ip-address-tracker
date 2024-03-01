4b8cf3e8da5a7853c02ccc9bca37f699244f4c5b
Gaspare Tortora <g.tortora2@gmail.com>
2024-02-29 22:37:53 -0500

    Removed axios call and moved it to its own component

--- src/components/Ip.jsx
+++ src/components/Ip.jsx
@@ -1,49 +1,11 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-const Ip =  ({ipAddress, err}) =>  {
-
-    const [response, setResponse] = useState({});
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
-
-    // wrapping API call to get IP address in useEffect
-    useEffect(() => {
-        const fetchData = async () => {
-                setLoading(true)
-                const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_IP_API_KEY}`;
-                axios.get(url, {
-                    params: {
-                        ipAddress: ipAddress
-                    }
-                }).
-                then(response => {
-                    setError("")
-                    setLoading(false);
-                    setResponse(response.data)
-                }).catch((error) => {
-                    console.log("something")
-                    setError(error.response.data.messages)
-                }).finally(() => {
-                    setLoading(false);
-                })
-        }
-        if (ipAddress) {
-            fetchData()
-        }
-    }, [ipAddress])
+const Ip =  ({ipAddress, response, error, loading}) =>  {
 
     if (loading) {
         return <p>Loading...</p>;
       }
-    
       
     if (error) {
         return <p>{error}</p>
-    }
-
-    if (err) {
-        return <p>{err}</p>
     }
     
     if (!ipAddress && !error) {
