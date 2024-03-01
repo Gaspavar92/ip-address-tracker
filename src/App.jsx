dde81fce86724c187b98ee549af9009684915d08
Gaspare Tortora <g.tortora2@gmail.com>
2024-03-01 17:05:40 -0500

    Improved styiling

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <FetchData setError={setError} setResponse={setResponse} setLoading={setLoading} ipAddress={ipAddress} err={error}/>
     <div className="flex flex-col h-screen">
       <SearchBar setIpAddress={setIpAddress} setError={setError} />
-      <div className="flex gap-24 bg-white py-16 px-32 absolute left-1/2 top-40 -translate-x-1/2 rounded-xl shadow-lg w-[70vw] min-w-fit justify-center z-10">
+      <div className="flex bg-white py-16 px-32 absolute left-1/2 top-40 -translate-x-1/2 rounded-xl shadow-lg w-[70vw] min-w-fit justify-between z-10">
         <Ip ipAddress={ipAddress} loading={loading} error={error} response={response}/>
       </div>
     <Map data={response}/>
--- src/components/Ip.jsx
+++ src/components/Ip.jsx
@@ -1,25 +1,37 @@
 const Ip =  ({ipAddress, response, error, loading}) =>  {
 
     if (loading) {
-        return <p>Loading...</p>;
+        return <p className="flex justify-center w-full">Loading...</p>;
       }
       
     if (error) {
-        return <p>{error}</p>
+        return <p className="flex justify-center w-full">{error}</p>
     }
     
     if (!ipAddress && !error) {
-        return <p>Look for an IP address to populate results.</p>
+        return <p className="flex justify-center w-full">Look for an IP address to populate results.</p>
     }
 
         return (
           
      response.location && (
             <>
-                <p className="border-r border-gray pr-16">Country: {response.location.country}</p>
-                <p className="border-r border-gray pr-16">City: {response.location.city}</p>
-                <p className="border-r border-gray pr-16">Region: {response.location.region}</p>
-                <p>ISP: {response.isp}</p>
+                <div className="country border-r border-gray pr-16 flex flex-col justify-between gap-4">
+                    <h2 className="text-gray-400 text-xs">COUNTRY</h2>
+                    <p className="text-xl">{response.location.country}</p>
+                </div>
+                <div className="city border-r border-gray pr-16 flex flex-col justify-between gap-4">
+                    <h2 className="text-gray-400 text-xs">CITY</h2>
+                    <p className="text-xl">{response.location.city}</p>
+                </div>
+                <div className="region border-r border-gray pr-16 flex flex-col justify-between gap-4">
+                    <h2 className="text-gray-400 text-xs">REGION</h2>
+                    <p className="text-xl"> {response.location.region}</p>
+                </div>
+                <div className="isp flex flex-col justify-between gap-4">
+                    <h2 className="text-gray-400 text-xs">ISP</h2>
+                    <p className="text-xl">{response.isp}</p>
+                </div> 
             </>
       )
           
